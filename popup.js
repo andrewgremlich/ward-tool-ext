10e962135b1d2fb8c900aec952593be1e1df7766
andrewgremlich <andrew.gremlich@gmail.com>
2019-03-17 20:55:38 -0600

    First testing version.

---
+++ popup.js
@@ -0,0 +1,16 @@
+document.querySelector("#download").onclick = () => {
+
+  // chrome.tabs.query({
+  //   active: true,
+  //   currentWindow: true
+  // }, (tabs) => {
+  //   chrome.tabs.sendMessage(tabs[0].id, {
+  //     greeting: "hello"
+  //   }, (resp) => {
+  //     console.log(resp.farewell);
+  //   })
+  // })
+
+  console.log("hello world!");
+
+}
