7e44bc518ca676b3d671e4cad7dddd44036a6881
Andrew Jacob Gremlich <andrew.gremlich@churchofjesuschrist.org>
2019-07-01 09:09:58 -0600

    Updated URL and query selector

--- downloadwarddirectory.js
+++ downloadwarddirectory.js
@@ -1,6 +1,6 @@
 console.log('The ward tools extension has been initiated.  Good to go!');
 
-const BarButtons = document.querySelector("[class*='BarButtons']");
+const BarButtons = document.querySelector("main nav nav");
 
 const downloadLink = document.createElement("a"),
   ExtensionButton = document.createElement("button");
@@ -82,7 +82,7 @@ const fetchWard = data => {
   console.log("Got user info!");
 
   let homeUnitId = data.homeUnits[0];
-  let unitString = `https://directory.lds.org/api/v4/households?unit=${homeUnitId}`;
+  let unitString = `https://directory.churchofjesuschrist.org/api/v4/households?unit=${homeUnitId}`;
 
   console.log("Your unit Id is " + homeUnitId);
   console.log("API URL is " + unitString);
@@ -94,7 +94,7 @@ const fetchWard = data => {
 }
 
 const fetchUserInfo = () => {
-  fetch("https://directory.lds.org/api/v4/user")
+  fetch("https://directory.churchofjesuschrist.org/api/v4/user")
     .then(d => d.json())
     .then(fetchWard)
     .catch(err => console.warn(err));
